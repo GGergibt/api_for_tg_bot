798a25e5ee49af65caf6846389d92f051afea1d0
GGergibt <76064583+GGergibt@users.noreply.github.com>
2022-05-19 12:37:34 +0300

    Update database_requests.py

--- database_requests.py
+++ database_requests.py
@@ -6,7 +6,7 @@ from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 config = dict(dotenv.dotenv_values(".env"))
 password = next(iter(config.values()))
 connection_to_database = psycopg2.connect(
-    host="localhost", database="personal_chat_history", user="gosha", password=password
+    host="localhost", database="personal_chat_history", user="www", password=password
 )
 connection_to_database.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 cursor = connection_to_database.cursor()
